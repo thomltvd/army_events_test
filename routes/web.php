5cab50da71e1cd2bb6caaae67d4ba241681e8843
thomltvd <thom.donina5@gmail.com>
2021-02-12 14:22:05 +0800

    added a comment

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//EDITED
 Route::get('/', function () {
     return view('welcome');
 });
